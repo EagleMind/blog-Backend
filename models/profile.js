76433e72c4e673357ff1e1b4e0c81342c71511fe
EagleMind <hassen.ben.mbarek@gmail.com>
2022-03-30 02:37:16 +0200

    added profile model

---
+++ models/profile.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const Profile = new schema({
+  name: { type: String },
+  profile_pic: { type: String },
+  bio: { type: String },
+  job_title: { type: String },
+  social_media: [
+    { name: "Linkedin", link: String },
+    { name: "Behance", link: String },
+    { name: "artstation", link: String },
+  ],
+  posts: [{ postId: String }],
+  following: [{ profileId: String }],
+});
+
+module.exports = ProfileModel = mongoose.model("profile", Profile);
